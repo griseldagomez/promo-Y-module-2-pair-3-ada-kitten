3786e03911b4a34c2b86f771489182411c42df34
griseldagomez <grinoe123@gmail.com>
2024-06-21 11:24:36 +0200

    js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const kittenOne = '<li class="card">
+    < article >
+         <img
+            class="card_img"
+            src="https://dev.adalab.es/gato-siames.webp"
+            alt="siames-cat"
+              />
+        <h3 class="card_title">Anastacio</h3>
+        <h4 class="card_race">Siamés</h4>
+              <p class="card_description">
+                Porte elegante, su patrón de color tan característico y sus ojos
+                de un azul intenso, pero su historia se remonta a Asía al menos
+                hace 500 años, donde tuvo su origen muy posiblemente.
+              </p>
+            </article >
+          </li > ';
